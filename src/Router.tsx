68a708ec7f9207c2b8155ddecae723b8e2a2162e
Stevan <jesus.diaz10@unmsm.edu.pe>
2023-11-12 19:25:31 -0500

    hola

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,10 +1,24 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { HomePage } from './pages/Home.page';
+import { HomePage } from './pages/Home';
+import { CalendarPage } from './pages/Calendar';
+import { Checkboxes } from './pages/Checkboxes';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <HomePage />,
+  },
+  {
+    path: '/calendario',
+    element: <CalendarPage />,
+  },
+  {
+    path: '/checkboxes',
+    element: <Checkboxes />,
+  },
+  {
+    path: '*',
+    element: <div>Not Found</div>,
   },
 ]);
 
