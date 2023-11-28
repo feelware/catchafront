5efcd76184490be1ad017906df11ab7f1b69c4b6
stevan <stevanpersonal@gmail.com>
2023-11-28 08:25:12 -0500

    Update Dashboard.tsx

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,11 +1,25 @@
 import { useUser } from "../../stores/userStore"
+import { useEffect, useState } from "react"
+import ActivityCalendar from 'react-activity-calendar'
+import { ToolTip } from "@mantine/core"
 
 import styles from "./Dashboard.module.css"
 
 export default function Dashboard() {
-  return (
-    <main>
+  const { user } = useUser()
 
-    </main>
+  return (
+    <main>      
+<ActivityCalendar
+  data={data}
+  renderBlock={(block, activity) => (
+    <Tooltip
+      title={`${activity.count} activities on ${activity.date}`}
+    >
+      {block}
+    </Tooltip>
+  )}
+/>    
+</main>
   )
 }
